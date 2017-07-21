6db352dc098d86ab348015c1887890e4841103c2
dsahapersonal <dsahapersonal@gmail.com>
2017-07-21 20:06:07 +0530

    another chanes

--- app/src/main/java/com/example/mkz/dataz/MainActivity.java
+++ app/src/main/java/com/example/mkz/dataz/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_main);
 
         Thread divert = new Thread();
 
