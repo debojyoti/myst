4e3e70850d334d392f37f82ef8472f3736426262
dsahapersonal <dsahapersonal@gmail.com>
2017-07-21 14:12:57 +0530

    another chanes

--- app/src/main/java/com/example/mkz/dataz/Launch.java
+++ app/src/main/java/com/example/mkz/dataz/Launch.java
@@ -9,10 +9,6 @@ package com.example.mkz.dataz;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class Launch extends AppCompatActivity {
 
@@ -45,7 +41,9 @@ public class Launch extends AppCompatActivity {
                     Intent i = new Intent(Launch.this,Recharge.class);
 
                     startActivity(i);
-                    finish();
+
+
+                    finish();   //  while it redirect to new activity , this (current) activity will be destroyed : @ Debojyoti
                 }
                 catch(Exception e)
                 {
